61e5e644773bd6849ce047c0b0f552bd57305ed5
brenhr <36976556+brenhr@users.noreply.github.com>
2022-06-09 11:11:28 -0500

    Password login
    Implemented password login

--- lib/main.dart
+++ lib/main.dart
@@ -28,40 +28,122 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'PetSnap',
       theme: ThemeData(colorScheme: ColorScheme.light()),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  bool _obscureText = true;
+  bool _passwordVisible = false;
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    emailController.dispose();
+    passController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    init();
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(
-          Icons.menu,
-        ),
-        title: Text('SnapPet'),
-        actions: [
-          Icon(Icons.favorite),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Icon(Icons.search),
-          ),
-          Icon(Icons.more_vert),
-        ],
+        title: Text('PetSnap'),
         backgroundColor: Colors.lightBlue[900],
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                signInAnonymously();
-              },
-              child: Text('Login Anonymously')
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Text(
+                'Login to PetSnap',
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: CircleAvatar(
+                radius: 110,
+                backgroundImage: NetworkImage(
+                  'https://firebasestorage.googleapis.com/v0/b/petsnapmx.appspot.com/o/assets%2Fduck-animate-1-500x500.png?alt=media&token=b5ba646a-adc5-4529-89b3-accc4a02eef4'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? value) {
+                  if (value!.trim().isEmpty) {
+                    return 'Email is required';
+                  }
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: passController,
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible ? Icons.visibility: Icons.visibility_off,
+                      color: Theme.of(context).primaryColorDark,
+                    ),
+                    onPressed: () {
+                      _obscureText: false;
+                      _passwordVisible = !_passwordVisible;
+                    },
+                  ),
+                ),
+                validator: (String? value) {
+                  if (value!.trim().isEmpty) {
+                    return 'Password is required';
+                  }
+                },
+              ),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.lightBlue[900],
+                    ),
+                    onPressed: () {
+                      signInWithEmailAndPassword(emailController.text, passController.text);
+                    },
+                    child: Text('Login')
+                  )
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.lightBlue[900],
+                    ),
+                    onPressed: () {
+                      registerAccount(emailController.text, passController.text);
+                    },
+                    child: Text('Register account')
+                  )
+                ),
+              ]
             )
           ]
         )
@@ -90,6 +172,49 @@ class HomePage extends StatelessWidget {
     }
   }
 
+  Future <void> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password
+      );
+      var uid = credential.user?.uid;
+      print("Signed in with id: $uid");
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  Future <void> registerAccount(String email, String password) async {
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      var uid = credential.user?.uid;
+      print("Signed in with id: $uid");
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> init() async {
+    print("Init app PetSnap...");
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
   void signOut() {
     FirebaseAuth.instance.signOut();
   }
@@ -101,6 +226,7 @@ class ApplicationState extends ChangeNotifier {
   }
 
   Future<void> init() async {
+    print("Init app PetSnap...");
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
