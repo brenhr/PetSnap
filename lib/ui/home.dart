3a3800d4ab191486ed9bd56e3f43acf6a3fc220e
Ricardo Taboada Magallanes <tabo.odricar@gmail.com>
2022-06-08 19:50:57 -0500

    [ADDED] Simple, very simple home page app.
    This is the principal view of the application. It need to have a camera
    preview, but since it require to have access to a Cameras, it is
    necessary to implement logic beside this.
    Fourtunally I found the following documentation.
    
    https://docs.flutter.dev/development/data-and-backend/state-mgmt/simple
    
    About how to implement a simple state for the App.

---
+++ lib/ui/home.dart
@@ -0,0 +1,77 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+// class HomeScreen extends StatelessWidget {
+//   static const route = '/home';
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return HomePage();
+//   }
+//
+// }
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  var _scaffoldKey = GlobalKey<ScaffoldState>();
+  late TabController _tabController;
+
+  @override
+  initState() {
+    super.initState();
+    _scaffoldKey = GlobalKey<ScaffoldState>();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).backgroundColor,
+        centerTitle: true,
+        title: Text("PetSnap"),
+      ),
+      // body: Text("Hello World!"),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Text("EntriesPage"),
+          Text("StatisticsPage"),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Snap',
+        isExtended: true,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        child: Icon(Icons.photo_camera),
+        //TODO onPressed functionality
+        onPressed: () {},
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 5.0,
+        clipBehavior: Clip.antiAlias,
+        shape: CircularNotchedRectangle(),
+        child: Material(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          child: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.receipt)),
+              Tab(icon: Icon(Icons.insert_chart)),
+            ],
+            // TODO: Tabs controller
+            controller: _tabController,
+            labelColor: Colors.indigo,
+            indicatorColor: Colors.white,
+            unselectedLabelColor: Colors.blueGrey,
+          ),
+        ),
+      ),
+    );
+  }
+}
